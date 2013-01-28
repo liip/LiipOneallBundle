fa5e5880de0a3a94e3a80dfc75e87a5e3d6ae586
Marco Bohler <marco.bohler@liip.ch>
2013-01-28 18:58:05 +0100

    Create new user/update user with new data from oneall

--- Security/User/UserProvider.php
+++ Security/User/UserProvider.php
@@ -10,18 +10,21 @@ use Liip\OneallBundle\Oneall\OneallApi;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class UserProvider implements UserProviderInterface
 {
     protected $oneall;
     protected $userManager;
     protected $validator;
+    protected $session;
 
-    public function __construct(OneallApi $oneall, UserManagerInterface $userManager, Validator $validator)
+    public function __construct(OneallApi $oneall, UserManagerInterface $userManager, Validator $validator, ContainerInterface $container)
     {
         $this->oneall = $oneall;
         $this->userManager = $userManager;
         $this->validator = $validator;
+        $this->session = $container->get('request')->getSession();
     }
 
     public function supportsClass($class)
@@ -29,39 +32,45 @@ class UserProvider implements UserProviderInterface
         return $this->userManager->supportsClass($class);
     }
 
-    public function findUserByOneId($uid)
+    public function findUserByOneAllId($uid)
     {
         return $this->userManager->findUserBy(array('oneallId' => $uid));
     }
 
     public function loadUserByUsername($username)
     {
-        $user = $this->findUserByOneId($username);
+        $user = $this->findUserByOneAllId($username);
 
         try {
             $userdata = $this->oneall->getUserData($username);
         } catch (OneallApiException $e) {
             $userdata = null;
         } catch (\Exception $e) {
-            var_dump($e->getMessage()); die;
+            $this->session->setFlash("Userdata could not be loaded");
         }
 
         if (!empty($userdata)) {
+            $network = array_shift($userdata['networks']);
+
             if (empty($user)) {
                 $user = $this->userManager->createUser();
+                $user->setOneallId($username);
                 $user->setEnabled(true);
                 $user->setPassword('');
             }
 
-            // TODO subscribe to changes, so that we would not have to do this if the user already exists
-            $user->setUserData($userdata);
-/*
-            $validation = $this->validator->validate($user, 'Oneall');
-            if (count($validation)) {
-                // TODO: add flash message?
-                throw new UsernameNotFoundException('Oneall user could not be stored');
+            if (empty($network['email'])) {
+                $network['email'] = '';
             }
-*/
+
+            $user->setUserData($network);
+
+            $validation = $this->validator->validate($user, 'Oneall');
+
+            if (count($validation)) {
+                $this->session->setFlash("Username could not be stored");
+            }
+
             $this->userManager->updateUser($user);
         }
 
